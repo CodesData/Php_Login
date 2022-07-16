f333607d1d3af07924a1791283d52b0953ce04fc
CodesData <91758284+CodesData@users.noreply.github.com>
2022-07-16 20:12:10 +0100

    initial

---
+++ Database/login_user.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  Dim 06 déc. 2020 à 10:04
+-- Version du serveur :  10.1.38-MariaDB
+-- Version de PHP :  7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `admin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `login_user`
+--
+
+CREATE TABLE `login_user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `login_user`
+--
+
+INSERT INTO `login_user` (`id`, `name`, `user_name`, `password`) VALUES
+(1, 'administrateur', 'user_ad', '12345');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `login_user`
+--
+ALTER TABLE `login_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `login_user`
+--
+ALTER TABLE `login_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
